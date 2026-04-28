ffc4f278dd58a2d2589030d47c2144883cc3be22
wjiaxin853-ctrl <wjiaxin853@gmail.com>
2026-04-28 11:46:29 +0800

    增加终端环境变量配置

---
+++ env.remote.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Project-local cache locations on the remote 2TB disk.
+export HF_HOME=/media/admin204/2tbdisk2/wjx/minimind-cache/hf
+export HF_DATASETS_CACHE=/media/admin204/2tbdisk2/wjx/minimind-cache/hf/datasets
+export UV_CACHE_DIR=/media/admin204/2tbdisk2/wjx/minimind-cache/uv
+export PIP_CACHE_DIR=/media/admin204/2tbdisk2/wjx/minimind-cache/pip
+
